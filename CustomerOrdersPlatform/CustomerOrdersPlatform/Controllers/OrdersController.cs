d9cef4ddf78495c72a7789392d7b5a4f94b7a714
Collin Neill <Collin Neill>
2016-03-13 14:43:32 -0400

    cmn-0 add order

---
+++ CustomerOrdersPlatform/CustomerOrdersPlatform/Controllers/OrdersController.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using CustomerOrdersPlatform.Persistence.Orders;
+using CustomerOrdersPlatform.Persistence.OrderDetails;
+
+namespace CustomerOrdersPlatform.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public JsonResult GetOrders()
+        {
+            CustomerOrdersEntitiesOrders coe = new CustomerOrdersEntitiesOrders();
+            var result = coe.Orders.ToList();
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetOrderDetails()
+        {
+            CustomerOrdersEntitiesOrderDetails coe = new CustomerOrdersEntitiesOrderDetails();
+            var result = coe.Order_Details.ToList();
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
