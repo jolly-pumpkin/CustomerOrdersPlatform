4ac8a32925ecda27fb06b90594d8b21b0cffe58d
Collin Neill <Collin Neill>
2016-03-13 13:30:39 -0400

    cmn-0 add basic customer data flow. entity -> ctrl -> js ctrl -> view

---
+++ CustomerOrdersPlatform/CustomerOrdersPlatform/Controllers/CustomersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CustomerOrdersPlatform.Persistence.Customer;
+
+namespace CustomerOrdersPlatform.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public JsonResult GetLastCustomer()
+        {
+            CustomerOrdersEntities coe = new CustomerOrdersEntities();
+            var result = coe.Customers.ToList();
+            return Json(result[0], JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ CustomerOrdersPlatform/CustomerOrdersPlatform/Scripts/app/CustomerController.js
@@ -0,0 +1,18 @@
+﻿angular.module('MyApp') 
+.controller('CustomerController', function ($scope, CustomerService) { 
+    $scope.Contact = null;
+    CustomerService.GetLastContact().then(function (d) {
+        $scope.Customer = d.data; // Success
+    }, function () {
+        alert('Failed'); // Failed
+    });
+})
+.factory('CustomerService', function ($http) {
+    var fac = {};
+    fac.GetLastContact = function () {
+        return $http.get('/Customers/GetLastCustomer');
+    }
+    return fac;
+});
+ 
+ 
